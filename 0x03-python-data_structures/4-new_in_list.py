54d7e18ddedc655c7eee02ce880e06a68e9e0217
owensdng <owensdng@gmail.com>
2023-08-14 06:44:39 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,22 +1,12 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+
+
 def new_in_list(my_list, idx, element):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list
-    
-    # Create a copy of the original list to keep the original list unchanged
-    new_list = my_list.copy()
-    
-    # Replace the element at the specified index with the new element
-    new_list[idx] = element
-    
-    return new_list
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
-# Test the function
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = new_in_list(my_list, idx, new_element)
-
-    print(new_list)  # Output: [1, 2, 3, 9, 5]
-    print(my_list)   # Output: [1, 2, 3, 4, 5]
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
