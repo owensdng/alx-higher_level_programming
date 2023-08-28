58063071c412ad110e99e93bc14cdb081306485b
owensdng <owensdng@gmail.com>
2023-08-28 13:34:06 -0700

    0x05-python

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     """Print an integer with "{:d}".format().
 
@@ -12,22 +13,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (TypeError, ValueError):
-        return False
-
-if __name__ == "__main__":
-    value = 89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = -89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = "School"
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
+        return (False)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,15 +8,4 @@ def safe_print_division(a, b):
         div = None
     finally:
         print("Inside result: {}".format(div))
-    return div
-
-if __name__ == "__main__":
-    a = 12
-    b = 2
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
+    return (div)
