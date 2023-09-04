282a6ffada555154ea91459a31fb61ae0d58cd8e
owensdng <owensdng@gmail.com>
2023-09-04 05:20:50 -0700

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int, optional): The width of the rectangle. 
+            height (int, optional): The height of the rectangle.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The new width value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle.
+
+        Args:
+            value (int): The new height value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate and return the perimeter of the rectangle."""
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(['#' * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        """Return a string representation of the rectangle for eval()."""
+        return f"Rectangle({self.__width}, {self.__height})"
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,39 @@
+Python - More Classes and Objects
+This project explores object-oriented programming in Python and covers topics such as class methods, static methods, class vs. instance attributes, and the use of special methods like __str__ and __repr__.
+
+Tasks
+0. Simple rectangle
+
+An empty Python class defining a rectangle.
+1. Real definition of a rectangle
+
+Defines a rectangle class with private attributes width and height. Includes setters and getters, as well as error handling.
+2. Area and Perimeter
+
+Adds methods to calculate the area and perimeter of the rectangle.
+3. String representation
+
+Implements __str__ to print the rectangle as a string.
+4. Eval is magic
+
+Implements __repr__ to return a string representation of the rectangle.
+5. Detect instance deletion
+
+Defines a __del__ method that prints a message when a rectangle instance is deleted.
+6. How many instances
+
+Adds a class attribute to count instances and handle deletions.
+7. Change representation
+
+Introduces a class attribute to customize the symbol used for string representation.
+8. Compare rectangles
+
+Adds a static method to compare rectangles based on their areas.
+9. A square is a rectangle
+
+A class method to create square instances.
+10. N Queens
+
+Solves the N queens puzzle for placing non-attacking queens on an NxN chessboard.
+Usage
+Run ./101-nqueens.py N to find solutions for the N queens puzzle.
